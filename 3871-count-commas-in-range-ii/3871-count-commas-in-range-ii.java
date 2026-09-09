c29eae2decbdc079cbe9329035a679050841d648
Harish Motamarri <motamarriharish@gmail.com>
2026-09-09 22:36:34 +0530

    Time: 1 ms (99.36%), Space: 42.8 MB (37.18%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long countCommas(long n) {
+        long  start = 1000;
+        long res = 0;
+        while(start<=n){
+            res = res + n - start + 1;
+            start *= 1000;
+        }
+        return res;
+    }
+}
